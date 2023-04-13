6f0cf60c83c99b74f168f7c272634c9a9e035a41
Reu-Amissah <reujoyamissah@gmail.com>
2023-04-13 10:03:41 +0000

    creaate dark-mode functionality

--- country-api/src/Components/Header.js
+++ country-api/src/Components/Header.js
@@ -1,15 +1,24 @@
 import React from "react";
 import "../Styles/Header.css";
+import { useState } from "react";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  const handleClick = () => {
+    setDarkMode(!isDarkMode);
+    const rootElement = document.getElementById("body");
+    rootElement.classList.toggle("dark-mode");
+  };
+
   return (
     <header>
       <div className="header-container">
         <div className="header-items">
           <div className="header-title">Where in the world</div>
-          <div className="dark-mode-toggle">
+          <div className="toggle-button" onClick={handleClick}>
             <span class="material-symbols-outlined">dark_mode</span>
             Dark Mode
           </div>
