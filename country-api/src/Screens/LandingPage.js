a93d5beb8f712474a0bfdd1a0a8a1bd2cb36d64c
Reu-Amissah <reujoyamissah@gmail.com>
2023-04-14 10:55:08 +0000

    create landingPage markup

--- country-api/src/Screens/LandingPage.js
+++ country-api/src/Screens/LandingPage.js
@@ -2,7 +2,26 @@ import React from "react";
 import "../Styles/Landing.css";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <section className="landing-page">
+      <div className="search-div">
+        <input placeholder="Search for a country..."></input>
+        <select>
+          <option>Hello</option>
+          <option>Hi</option>
+        </select>
+      </div>
+      <div className="items-div">
+        <div className="item">item1</div>
+        <div className="item">item2</div>
+        <div className="item">item3</div>
+        <div className="item">item4</div>
+        <div className="item">item5</div>
+        <div className="item">item6</div>
+        <div className="item">item7</div>
+      </div>
+    </section>
+  );
 };
 
 export default LandingPage;
