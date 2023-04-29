9ce5c38fca91cb5a23ecc11be7e65695e2c5dd21
Reu-Amissah <reujoyamissah@gmail.com>
2023-04-29 09:31:10 +0000

    fetch api success

--- country-api/src/Screens/LandingPage.js
+++ country-api/src/Screens/LandingPage.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Styles/Landing.css";
 
 const LandingPage = () => {
+  // const response = fetch("https://restcountries.com/v3.1/all");
+  // const jsonData = response.json();
+  // useEffect(() => {
+  //   console.log(jsonData);
+  //   // fetch("https://restcountries.com/v3.1/all", {
+  //   //   method: "GET", // or 'PUT'
+  //   //   headers: {
+  //   //     "Content-Type": "application/json",
+  //   //   },
+  //   //   body: JSON.stringify(data),
+  //   // })
+  //   //   .then((response) => response.json())
+  //   //   .then((data) => {
+  //   //     console.log("Success:", data);
+  //   //   })
+  //   //   .catch((error) => {
+  //   //     console.error("Error:", error);
+  //   //   });
+  //   // getCountryInfo();
+  // });
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://restcountries.com/v3.1/all");
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  fetch("https://restcountries.com/v3.1/all")
+    .then((response) => response.json())
+    .then((responseJSON) => {
+      // do stuff with responseJSON here...
+      console.log(responseJSON);
+    })
+    .catch((error) => {
+      //handle error
+    });
+
+  // .then((data) => {
+  //   //handle data
+  //   console.log(data);
+  // })
   return (
     <section className="landing-page" id="landing">
       <div className="search-item">
@@ -19,6 +70,10 @@ const LandingPage = () => {
         <div className="item">item5</div>
         <div className="item">item6</div>
         <div className="item">item7</div>
+        {data.map((item, index) => (
+          <li key={index}>{item.capital}</li>
+        ))}
+        {/* <div className="item"></div> */}
       </div>
     </section>
   );
